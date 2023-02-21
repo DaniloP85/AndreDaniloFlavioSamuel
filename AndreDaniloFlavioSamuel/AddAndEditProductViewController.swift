efd70899223965d0a187f2d559764ed465004ffa
DaniloP85 <danilopsnts@gmail.com>
2023-02-21 20:01:58 -0300

    add class and coredata settings

---
+++ AndreDaniloFlavioSamuel/AddAndEditProductViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddAndEditProductViewController.swift
+//  AndreDaniloFlavioSamuel
+//
+//  Created by Danilo Santos on 21/02/23.
+//
+
+import UIKit
+
+class AddAndEditProductViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
